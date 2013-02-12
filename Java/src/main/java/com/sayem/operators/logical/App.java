9fea853d51657039b28e4cb0eb5a763d3dd4bf0e
sayems <syed@hotmail.com>
2013-02-12 13:40:47 -0500

    Modiied operators

---
+++ Java/src/main/java/com/sayem/operators/logical/App.java
@@ -0,0 +1,13 @@
+package com.sayem.operators.logical;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
