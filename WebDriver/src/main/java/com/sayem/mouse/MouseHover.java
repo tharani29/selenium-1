b30d042bff5c1802b8cadbe8e5d28556cd2166c1
Syed Sayem <syed@hotmail.com>
2013-02-11 22:17:47 -0500

    Modified MouseHover.java

--- WebDriver/src/main/java/com/sayem/mouse/MouseHover.java
+++ WebDriver/src/main/java/com/sayem/mouse/MouseHover.java
@@ -1,11 +1,8 @@
 package com.sayem.mouse;
 
-/**
- * Created with IntelliJ IDEA.
- * User: sayem
- * Date: 12/24/12
- * Time: 7:39 AM
- * To change this template use File | Settings | File Templates.
- */
 public class MouseHover {
+
+    public static void main(String [] args){
+
+    }
 }
