de9e36cce4605f749da975d2fa30306dea85e104
Syed Sayem <syed@hotmail.com>
2013-02-13 20:41:39 -0500

    Added assertion

---
+++ Java/src/main/java/com/sayem/assertion/part2/FirstScript.java
@@ -0,0 +1,17 @@
+package com.sayem.assertion.part2;
+
+public class FirstScript {
+
+    public static void main(String [] args){
+
+        int [] numbers = new int[10];
+
+        numbers[0] = 5;
+        numbers[1] = 8;
+
+        for (int n : numbers){
+            System.out.println(n);
+        }
+
+    }
+}
