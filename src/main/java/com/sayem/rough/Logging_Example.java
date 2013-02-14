da7c4d93e71c2b1e1a930d386a0228786920491a
sayems <syed@hotmail.com>
2013-02-14 17:11:39 -0500

    Added Logger

---
+++ src/main/java/com/sayem/rough/Logging_Example.java
@@ -0,0 +1,21 @@
+package com.sayem.rough;
+
+import org.apache.log4j.Logger;
+
+public class Logging_Example {
+
+	public static void main(String[] args) {
+
+		// add log4j.jar
+		// add log4j.properties directly inside the src folder
+		// create the object in the code
+		
+		Logger APPLICATION_LOGS = Logger.getLogger("devpinoyLogger");
+		APPLICATION_LOGS.debug("hello");
+		APPLICATION_LOGS.debug("We are wrinting in to a log file");
+		APPLICATION_LOGS.debug("starting the test case xyz test");
+
+		
+	}
+
+}
