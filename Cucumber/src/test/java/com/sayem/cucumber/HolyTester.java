07defee422bfe0af53e46f6ab1b7fef7fcd0a6d3
Syed Sayem <syed@hotmail.com>
2013-02-17 22:14:56 -0500

    Added Cucumber

---
+++ Cucumber/src/test/java/com/sayem/cucumber/HolyTester.java
@@ -0,0 +1,32 @@
+package com.sayem.cucumber;
+
+import cucumber.api.java.en.*;
+import cucumber.runtime.PendingException;
+
+public class HolyTester {
+    @Given("^I am a [a-zA-Z]{1,} tester$")
+    public void I_am_a_bad_tester(){
+        System.out.println("********************************");
+        System.out.println("@Given - I_am_a_bad_tester()");
+    }
+
+    @When("^I go to work$")
+    public void I_go_to_work(){
+        System.out.println("@When - I_go_to_work()");
+    }
+
+    @Then("^I [a-zA-Z]{1,} it$")
+    public void I_mess_with_it(){
+        System.out.println("@Then - I_mess_with_it()");
+    }
+
+    @And("^My boss [a-zA-Z]{1,} me$")
+    public void My_boss_fires_me(){
+        System.out.println("@And - My_boss_fires_me()");
+    }
+
+    @But("^The developer [a-zA-Z]{1,} me$")
+    public void The_developer_likes_me(){
+        System.out.println("@But - The_developer_likes_me()");
+    }
+}
---
+++ Cucumber/target/classes/com/sayem/cucumber/itemployee.feature
@@ -0,0 +1,21 @@
+Feature: Life of IT guys
+
+  In order to get good salary
+  As a IT guy
+  I want to keep my boss happy
+
+  Scenario: I am a bad tester
+    Given I am a bad tester
+    When I go to work
+    Then I mess it
+    And My boss fires me
+    But The developer likes me
+
+
+    Scenario: I am a good tester
+      Given I am a good tester
+      When I go to work
+      Then I complete it
+      And My boss salutes me
+      But The developer hates me
+
