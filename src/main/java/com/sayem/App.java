8a43ee3519705cba9e85b0398836f133aeca7cce
Syed Sayem <syed@hotmail.com>
2013-02-23 13:26:12 -0500

    Modified App.java

--- src/main/java/com/sayem/App.java
+++ src/main/java/com/sayem/App.java
@@ -1,13 +1,22 @@
 package com.sayem;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.iphone.IPhoneDriver;
+
 /**
- * Hello world!
+ * Aurthur: Syed Sayem
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App{
+    public static void main( String[] args ) throws Exception {
+
+        WebDriver driver = new IPhoneDriver();
+        driver.get("https://www.facebook.com/");
+        driver.findElement(By.name("email")).sendKeys("syed@hotmail.com");
+        driver.findElement(By.name("pass")).sendKeys("n4#m0n7#n83n8sn38&*30~@");
+        driver.findElement(By.name("login")).click();
+
     }
 }
