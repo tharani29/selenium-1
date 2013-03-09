f53374a674037dba55f09721c438ac8f4fba19b2
Syed Sayem <syed@hotmail.com>
2013-03-09 12:47:23 -0500

    Added calculator.java

---
+++ src/main/java/com/sayem/Calculator.java
@@ -0,0 +1,16 @@
+package com.sayem;
+
+public class Calculator {
+
+    public static void main(String [] args){
+
+        if (args.length != 1){
+            System.out.println("Usage: Calculator <expression>");
+            return;
+        }
+
+
+        String expression = args[0];
+
+    }
+}
