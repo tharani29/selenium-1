7dcaa9b81c06ba06b0a28e4e21b22b435c31580f
Syed Sayem <syed@hotmail.com>
2013-03-23 00:12:03 -0400

    Added Sample URL to Generic class

--- Java/src/main/java/com/sayem/generics/ABC.java
+++ Java/src/main/java/com/sayem/generics/ABC.java
@@ -1,5 +1,12 @@
 package com.sayem.generics;
 
+
+// http://www.java2s.com/Tutorial/Java/0200__Generics/Catalog0200__Generics.htm
+// http://code.stephenmorley.org/articles/java-generics-type-erasure/
+// http://www.programmingforfuture.com/2009/02/re-java-generic-examples-source-code.html
+// http://java2novice.com/java-generics/simple-class/
+// http://learnertobeginner.blogspot.com/2011/06/basics-of-java-generics.html
+
 public class ABC<T> {
 
     // Declares an object of type T
