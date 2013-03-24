c9366f8743776881fed46bfc5495cbbc3416f27e
Syed Sayem <syed@hotmail.com>
2013-03-24 13:15:11 -0400

    Added serialization

---
+++ Java/src/main/java/com/sayem/serialization/App.java
@@ -0,0 +1,13 @@
+package com.sayem.serialization;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
