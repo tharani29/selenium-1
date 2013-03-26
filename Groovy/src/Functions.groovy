721aff33b03588be7451b6479ea4ffdf39f0006c
sayems <syed@hotmail.com>
2013-03-26 15:57:31 -0400

    Fuctions was added to groovy repo

---
+++ Groovy/src/Functions.groovy
@@ -0,0 +1,18 @@
+//Employee.log = log
+
+Employee e1 = new Employee()
+Employee e2 = new Employee()
+e1.name = "A"
+e2.name = "B"
+e1.printName()
+e2.printName()
+
+class Employee{
+    def static log
+    def name
+
+    public void printName(){
+        println name
+    }
+
+}
