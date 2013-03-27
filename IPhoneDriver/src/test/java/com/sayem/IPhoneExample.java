c3d974b4089162156014d4ea427e20b0bdea094d
Syed Sayem <syed@hotmail.com>
2013-03-26 21:53:37 -0400

    Modified Instructions & IPhoneExample.java

--- IPhoneDriver/src/test/java/com/sayem/IPhoneExample.java
+++ IPhoneDriver/src/test/java/com/sayem/IPhoneExample.java
@@ -19,17 +19,23 @@
 package com.sayem;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.iphone.IPhoneDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 public class IPhoneExample {
     public static void main( String[] args ) throws Exception {
 
-        WebDriver driver = new IPhoneDriver("http://192.168.1.148:3001/wd/hub");
-        driver.get("https://www.facebook.com/");
-        driver.findElement(By.name("email")).sendKeys("syed@hotmail.com");
-        driver.findElement(By.name("pass")).sendKeys("s@m13o9z");
-        driver.findElement(By.name("login")).click();
+        //WebDriver driver = new IPhoneDriver("http://192.168.1.148:3001/wd/hub");
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.1.148:3001/wd/hub"), DesiredCapabilities.iphone());
+        driver.get("https://www.totsy.com/");
+        driver.findElement(By.cssSelector("#email")).sendKeys("sssayem@totsy.com");
+        driver.findElement(By.cssSelector("#pass")).sendKeys("user123");
+        driver.findElement(By.cssSelector("#submit-button")).click();
 
     }
 }
