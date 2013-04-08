55eb780a36d62ff591a7e72569b7e05267bdeb47
Syed Sayem <syed@hotmail.com>
2013-04-08 10:55:49 -0400

    Added dog objects class to Head First Java Modules

---
+++ src/main/java/com/sayem/dog/DogTestDrive.java
@@ -0,0 +1,11 @@
+package com.sayem.dog;
+
+public class DogTestDrive {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.size = 40;
+        d.bark();
+    }
+}
