b8ddffaa543985d28f40943338e8cdb13dc0446a
Syed Sayem <syed@hotmail.com>
2013-04-07 21:47:36 -0400

    Added PhraseOMatic class to the Head First Java

---
+++ src/main/java/com/sayem/PhraseOMatic.java
@@ -0,0 +1,14 @@
+package com.sayem;
+
+public class PhraseOMatic {
+    public static void main(String[] args) {
+        // Make three sets of words to choose from. Add your own!!
+        String [] pets = {"Fido", "Zeus", "Bin"};
+
+        String s = pets[0];
+        s = s + " " + "is a dog";
+
+        System.out.println(s);
+
+    }
+}
