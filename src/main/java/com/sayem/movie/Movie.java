e8ff43631a68149c3943e69a45cd6fb2cdb7d000
Syed Sayem <syed@hotmail.com>
2013-04-10 12:16:13 -0400

    Changed Selenium to latest verson 2.32.0

---
+++ src/main/java/com/sayem/movie/Movie.java
@@ -0,0 +1,11 @@
+package com.sayem.movie;
+
+public class Movie {
+    String title;
+    String genre;
+    int rating;
+
+    public void playIt(){
+        System.out.println("Playing the movie");
+    }
+}
