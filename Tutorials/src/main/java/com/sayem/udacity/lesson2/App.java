e91b8a7d2f26ca87e75ec040061197bc99bbc266
Syed Sayem <syed@hotmail.com>
2013-07-17 22:10:49 -0400

    Modified udacity

---
+++ Tutorials/src/main/java/com/sayem/udacity/lesson2/App.java
@@ -0,0 +1,13 @@
+package com.sayem.udacity.lesson2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
