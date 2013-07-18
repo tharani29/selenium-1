7bee1e7657eb3e6b4399227a9ea9507ff965e8b1
Syed Sayem <syed@hotmail.com>
2013-07-18 19:53:26 -0400

    Modified udacity

---
+++ Tutorials/src/main/java/com/sayem/udacity/lesson1/HelloPrinter.java
@@ -0,0 +1,7 @@
+package com.sayem.udacity.lesson1;
+
+public class HelloPrinter {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Tutorials/src/main/java/com/sayem/udacity/lesson1/TextAndNumbers.java
@@ -0,0 +1,8 @@
+package com.sayem.udacity.lesson1;
+
+public class TextAndNumbers {
+    public static void main(String[] args) {
+        System.out.println(3 + 4 + 5);
+        System.out.println("3 + 4 + 5");
+    }
+}
