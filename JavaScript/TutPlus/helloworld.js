cc52a04ef57ba4986b93a845408c90bc98ebcad7
Syed Sayem <syed@hotmail.com>
2013-07-21 14:16:31 -0400

    Modified JavaScript modules

---
+++ JavaScript/TutPlus/helloworld.js
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: sayem
+ * Date: 7/21/13
+ * Time: 1:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+alert("Hello World");
