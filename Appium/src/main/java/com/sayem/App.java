c1f366ebdf364c2c2f696205d382599ac703aa12
Syed Sayem <syed@hotmail.com>
2013-08-12 18:20:13 -0400

    Modified Appium modules

--- Appium/src/main/java/com/sayem/App.java
+++ Appium/src/main/java/com/sayem/App.java
@@ -1,13 +1,46 @@
 package com.sayem;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class App {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        // set up appium
+        File app = new File("/Users/ssayem/Downloads/LearnVest.app");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "iOS");
+        capabilities.setCapability(CapabilityType.VERSION, "6.0");
+        capabilities.setCapability(CapabilityType.PLATFORM, "Mac");
+        capabilities.setCapability("app", app.getAbsolutePath());
+        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void testUIComputation() throws Exception {
+        driver.findElement(By.xpath("//window[1]/button[1]")).click();
+
+
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        //driver.quit();
     }
 }
