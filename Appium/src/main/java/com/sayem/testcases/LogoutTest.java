8672689f5d3d3964e9c92d8e7023ad75a58e77b8
Syed Sayem <syed@hotmail.com>
2013-08-13 00:15:54 -0400

    Added LogoutTest.class

---
+++ Appium/src/main/java/com/sayem/testcases/LogoutTest.java
@@ -0,0 +1,11 @@
+package com.sayem.testcases;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sayem
+ * Date: 8/13/13
+ * Time: 12:15 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LogoutTest {
+}
