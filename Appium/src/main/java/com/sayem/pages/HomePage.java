1d1244b63e9aced58a075f7e2e42ea51857fc7c8
Syed Sayem <syed@hotmail.com>
2013-08-13 00:00:28 -0400

    Added FinancialConsultationPage.java, LearnvastArticlesPage.java, MoneyCenterPage.java

--- Appium/src/main/java/com/sayem/pages/HomePage.java
+++ Appium/src/main/java/com/sayem/pages/HomePage.java
@@ -16,19 +16,32 @@ public class HomePage {
     @FindBy(xpath = "//window[1]/button[1]")
     private WebElement moneyCenter;
 
-    @FindBy(xpath = "//window[1]/button[1]")
+    @FindBy(xpath = "//window[1]/button[3]")
     private WebElement financialConsultation;
 
-    @FindBy(xpath = "//window[1]/button[1]")
+    @FindBy(xpath = "//window[1]/button[6]")
     private WebElement learnVestArticles;
 
-    @FindBy(xpath = "//window[1]/button[1]")
+    @FindBy(xpath = "//window[1]/button[13]")
     private WebElement Settings;
 
-    public SettingsPage signUpAndLogin(){
-        return PageFactory.initElements(driver, SettingsPage.class);
-
+    public MoneyCenterPage goToMoneyCenter(){
+        moneyCenter.click();
+        return PageFactory.initElements(driver, MoneyCenterPage.class);
     }
 
+    public FinancialConsultationPage goToFinancialConsultation(){
+        financialConsultation.click();
+        return PageFactory.initElements(driver, FinancialConsultationPage.class);
+    }
 
+    public LearnvastArticlesPage goToLearnVestArticles(){
+        learnVestArticles.click();
+        return PageFactory.initElements(driver, LearnvastArticlesPage.class);
+    }
+
+    public SettingsPage goToSettingsPage(){
+        Settings.click();
+        return PageFactory.initElements(driver, SettingsPage.class);
+    }
 }
---
+++ Appium/src/main/java/com/sayem/pages/MoneyCenterPage.java
@@ -0,0 +1,11 @@
+package com.sayem.pages;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sayem
+ * Date: 8/12/13
+ * Time: 11:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MoneyCenterPage {
+}
