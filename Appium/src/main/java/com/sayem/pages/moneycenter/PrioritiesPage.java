d4047dd275272521ed4af39955ede2f98406f8a5
Syed Sayem <syed@hotmail.com>
2013-08-14 17:44:59 -0400

    Added PriorityPage.java

---
+++ Appium/src/main/java/com/sayem/pages/moneycenter/PrioritiesPage.java
@@ -0,0 +1,11 @@
+package com.sayem.pages.moneycenter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ssayem
+ * Date: 8/14/13
+ * Time: 5:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PrioritiesPage {
+}
