469bef08ea1c90e5f85679f3f518051bd55c0abe
Syed Sayem <syed@hotmail.com>
2013-08-14 17:45:19 -0400

    Added AccountsPage.java

---
+++ Appium/src/main/java/com/sayem/pages/moneycenter/AccountsPage.java
@@ -0,0 +1,11 @@
+package com.sayem.pages.moneycenter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ssayem
+ * Date: 8/14/13
+ * Time: 5:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AccountsPage {
+}
