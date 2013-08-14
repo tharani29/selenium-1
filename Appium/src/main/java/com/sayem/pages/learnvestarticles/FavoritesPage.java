942b7bb9711877d115fc9c1d71ef8447990ddf5d
Syed Sayem <syed@hotmail.com>
2013-08-14 17:48:29 -0400

    Added FavoritePage.java

---
+++ Appium/src/main/java/com/sayem/pages/learnvestarticles/FavoritesPage.java
@@ -0,0 +1,11 @@
+package com.sayem.pages.learnvestarticles;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ssayem
+ * Date: 8/14/13
+ * Time: 5:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FavoritesPage {
+}
