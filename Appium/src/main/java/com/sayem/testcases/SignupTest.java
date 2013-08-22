39675686a2023c764c11c869aac39d3f115436c3
Syed Sayem <syed@hotmail.com>
2013-08-22 14:15:06 -0400

    Refector SignupTest class

--- Appium/src/main/java/com/sayem/testcases/SignupTest.java
+++ Appium/src/main/java/com/sayem/testcases/SignupTest.java
@@ -16,21 +16,12 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 
-public class SignupTest {
+public class SignupTest extends TestBase{
     private WebDriver driver;
 
     @BeforeMethod
     public void setUp() throws Exception {
-        // set up appium
-        File app = new File("/Users/ssayem/Downloads/LearnVest.app");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(CapabilityType.BROWSER_NAME, "iOS");
-        capabilities.setCapability(CapabilityType.VERSION, "6.0");
-        capabilities.setCapability(CapabilityType.PLATFORM, "Mac");
-        capabilities.setCapability("app", app.getAbsolutePath());
-        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-
+        initDriver();
     }
 
     @Test
