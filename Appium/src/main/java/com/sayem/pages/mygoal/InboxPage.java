143f37413ec1dc9f2f52baec88f29fa2c8b9a3e8
Syed Sayem <syed@hotmail.com>
2013-08-23 14:53:02 -0400

    Modified InboxPage.java

--- Appium/src/main/java/com/sayem/pages/mygoal/InboxPage.java
+++ Appium/src/main/java/com/sayem/pages/mygoal/InboxPage.java
@@ -1,11 +1,4 @@
 package com.sayem.pages.mygoal;
 
-/**
- * Created with IntelliJ IDEA.
- * User: ssayem
- * Date: 8/23/13
- * Time: 2:50 PM
- * To change this template use File | Settings | File Templates.
- */
 public class InboxPage {
 }
