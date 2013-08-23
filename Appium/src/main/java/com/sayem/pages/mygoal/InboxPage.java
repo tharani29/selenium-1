373e7b1702019d3af5413dd4fae456cb59aa3e21
Syed Sayem <syed@hotmail.com>
2013-08-23 14:51:05 -0400

    Added mygoal package and InboxPage to the project

---
+++ Appium/src/main/java/com/sayem/pages/mygoal/InboxPage.java
@@ -0,0 +1,11 @@
+package com.sayem.pages.mygoal;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ssayem
+ * Date: 8/23/13
+ * Time: 2:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class InboxPage {
+}
