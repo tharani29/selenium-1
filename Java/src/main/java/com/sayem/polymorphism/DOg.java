7ce703342409cf980184d0d79cb21573d8cffb26
Syed Sayem <syed@hotmail.com>
2013-08-24 13:19:59 -0400

    Added Dog class to polypmorphism package

--- Java/src/main/java/com/sayem/polymorphism/DOg.java
+++ Java/src/main/java/com/sayem/polymorphism/DOg.java
@@ -1,11 +1,7 @@
 package com.sayem.polymorphism;
 
-/**
- * Created with IntelliJ IDEA.
- * User: sayem
- * Date: 8/24/13
- * Time: 1:16 PM
- * To change this template use File | Settings | File Templates.
- */
-public class DOg {
+public class Dog extends Animal{
+    public void talk(){
+        System.out.println("Woof!");
+    }
 }
