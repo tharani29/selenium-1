120c889955c01ee3bcaf69b255a078ddbc518844
Syed Sayem <syed@hotmail.com>
2013-08-24 13:19:42 -0400

    Added Animal class to polypmorphism package

--- Java/src/main/java/com/sayem/polymorphism/Animal.java
+++ Java/src/main/java/com/sayem/polymorphism/Animal.java
@@ -1,11 +1,7 @@
 package com.sayem.polymorphism;
 
-/**
- * Created with IntelliJ IDEA.
- * User: sayem
- * Date: 12/24/12
- * Time: 8:02 AM
- * To change this template use File | Settings | File Templates.
- */
-public class Animal {
+public abstract class Animal {
+    public void talk(){
+        System.out.println("Hi, there.");
+    }
 }
