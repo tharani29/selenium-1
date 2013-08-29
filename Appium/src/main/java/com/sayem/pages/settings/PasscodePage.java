a2e4d6bd31171730e9deb756a88a463e2ecb7928
Syed Sayem <syed@hotmail.com>
2013-08-29 17:28:01 -0400

    Added passcode & changePasscode Methods

--- Appium/src/main/java/com/sayem/pages/settings/PasscodePage.java
+++ Appium/src/main/java/com/sayem/pages/settings/PasscodePage.java
@@ -1,11 +1,24 @@
 package com.sayem.pages.settings;
 
-/**
- * Created with IntelliJ IDEA.
- * User: ssayem
- * Date: 8/29/13
- * Time: 4:31 PM
- * To change this template use File | Settings | File Templates.
- */
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class PasscodePage {
+
+    private WebDriver driver;
+
+    public PasscodePage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    @FindBy(xpath = "//window[1]/toolbar[1]/button[2]")
+    private WebElement accountPlusSign;
+    public void passcode(){
+
+    }
+
+    public void changePasscode(){
+
+    }
 }
