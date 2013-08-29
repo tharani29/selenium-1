02fe1d23aafeedb8bc619ca82e21e613e37e7156
Syed Sayem <syed@hotmail.com>
2013-08-29 16:43:12 -0400

    Added WebElement identifier and AddAnAccount method

--- Appium/src/main/java/com/sayem/pages/settings/AccountPage.java
+++ Appium/src/main/java/com/sayem/pages/settings/AccountPage.java
@@ -1,4 +1,30 @@
 package com.sayem.pages.settings;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class AccountPage {
+
+    private WebDriver driver;
+
+    public AccountPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    @FindBy(xpath = "//window[1]/toolbar[1]/button[2]")
+    private WebElement accountPlusSign;
+
+    @FindBy(xpath = "//window[1]/textfield[1]")
+    private WebElement searchForAnAccount;
+
+    @FindBy(xpath = "//window[1]/tableview[1]/cell[1]/text[1]")
+    private WebElement popUpAccountSearchName;
+
+
+    public void addAnAccount(){
+        accountPlusSign.click();
+        searchForAnAccount.sendKeys("DAGBANK");
+        popUpAccountSearchName.click();
+    }
 }
