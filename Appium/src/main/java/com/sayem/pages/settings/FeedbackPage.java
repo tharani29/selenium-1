435b798dae66ffa74c5aa828dabafa27ceffbd3a
Syed Sayem <syed@hotmail.com>
2013-08-29 16:32:30 -0400

    Added FeedbackPage

---
+++ Appium/src/main/java/com/sayem/pages/settings/FeedbackPage.java
@@ -0,0 +1,11 @@
+package com.sayem.pages.settings;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ssayem
+ * Date: 8/29/13
+ * Time: 4:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FeedbackPage {
+}
