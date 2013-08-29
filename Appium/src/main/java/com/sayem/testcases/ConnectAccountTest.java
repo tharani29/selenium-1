1582007fbbb530e2220f4253223962535a844792
Syed Sayem <syed@hotmail.com>
2013-08-29 16:45:31 -0400

    Added TestNG test methods

--- Appium/src/main/java/com/sayem/testcases/ConnectAccountTest.java
+++ Appium/src/main/java/com/sayem/testcases/ConnectAccountTest.java
@@ -1,4 +1,26 @@
 package com.sayem.testcases;
 
+import com.sayem.pages.random.LandingPage;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class ConnectAccountTest extends TestBase{
+
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        initDriver();
+    }
+
+
+    @Test
+    protected void testUIComputation() throws Exception {
+
+        String emailAddress = "sayem@ilearnvest.com";
+        String password = "user1234";
+
+        LandingPage landingPage = PageFactory.initElements(driver, LandingPage.class);
+        landingPage.signUpAndLogin().login().loginIn(emailAddress, password);
+    }
 }
