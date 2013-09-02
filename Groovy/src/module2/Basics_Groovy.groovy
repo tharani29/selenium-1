cd1c51e5707b9e61d456ed87491bef067f471dc1
Syed Sayem <syed@hotmail.com>
2013-09-01 23:14:45 -0400

    Modified Basics_Groovy

--- Groovy/src/module2/Basics_Groovy.groovy
+++ Groovy/src/module2/Basics_Groovy.groovy
@@ -1,9 +1,3 @@
 package module2
 
-/**
- * Created with IntelliJ IDEA.
- * User: sayem
- * Date: 9/1/13
- * Time: 5:27 PM
- * To change this template use File | Settings | File Templates.
- */
+log.info "Hello world"
