4b4259016299a79f50fb9e2c99e733edb73657e4
Syed Sayem <syed@hotmail.com>
2013-09-07 12:36:59 -0400

    Added Numbers class and modified Chain class to Interface

--- DesignPatterns/src/main/java/com/sayem/derekbanas/chainofresponsibility/Chain.java
+++ DesignPatterns/src/main/java/com/sayem/derekbanas/chainofresponsibility/Chain.java
@@ -1,11 +1,8 @@
 package com.sayem.derekbanas.chainofresponsibility;
 
-/**
- * Created with IntelliJ IDEA.
- * User: sayem
- * Date: 9/7/13
- * Time: 12:32 PM
- * To change this template use File | Settings | File Templates.
- */
-public class Chain {
+public interface Chain {
+
+    public void setNextChain(Chain nextChain);
+
+    public void calculate(Numbers request);
 }
---
+++ DesignPatterns/src/main/java/com/sayem/derekbanas/chainofresponsibility/Numbers.java
@@ -0,0 +1,4 @@
+package com.sayem.derekbanas.chainofresponsibility;
+
+public class Numbers {
+}
