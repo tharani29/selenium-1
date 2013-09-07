493dfeb6f128ca0f393a23c400b40386d33dcb27
Syed Sayem <syed@hotmail.com>
2013-09-07 12:55:53 -0400

    Added SubtractNumbers too DesignPattern module

---
+++ DesignPatterns/src/main/java/com/sayem/derekbanas/chainofresponsibility/SubtractNumbers.java
@@ -0,0 +1,28 @@
+package com.sayem.derekbanas.chainofresponsibility;
+
+public class SubtractNumbers implements Chain{
+
+    private  Chain nextInChain;
+
+    @Override
+    public void setNextChain(Chain nextChain) {
+
+        nextInChain = nextChain;
+
+    }
+
+    @Override
+    public void calculate(Numbers request) {
+
+        if(request.getCalculationWanted() == "sub"){
+
+            System.out.print(request.getNumber1() + " - " + request.getNumber2() + " = "+
+                    (request.getNumber1()-request.getNumber2()));
+
+        } else {
+
+            nextInChain.calculate(request);
+
+        }
+    }
+}
