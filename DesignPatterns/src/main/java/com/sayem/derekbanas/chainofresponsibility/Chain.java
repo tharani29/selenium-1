61bea3fcd2504808547435c1b9818ff69d754da3
Syed Sayem <syed@hotmail.com>
2013-09-07 12:33:05 -0400

    Added Chain class to DesignPattern project

---
+++ DesignPatterns/src/main/java/com/sayem/derekbanas/chainofresponsibility/Chain.java
@@ -0,0 +1,11 @@
+package com.sayem.derekbanas.chainofresponsibility;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sayem
+ * Date: 9/7/13
+ * Time: 12:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Chain {
+}
